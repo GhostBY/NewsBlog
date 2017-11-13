6a5acd93e40474ff40688f1c5214ec47a4011ec8
Lukashov Alexandr <alexandrlukashov1997@gmail.com>
2017-11-13 21:23:08 +0300

    Add project files.

---
+++ NewsBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NewsBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

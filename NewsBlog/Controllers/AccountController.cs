5248f1efc13387a70c0ad889e78bf8b787004974
Lukashov Alexandr <alexandrlukashov1997@gmail.com>
2017-11-18 13:54:40 +0300

    add AccountController and authentication logic

---
+++ NewsBlog/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NewsBlog.Models.Entities;
+using NewsBlog.Models.ViewModel;
+
+namespace NewsBlog.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> userManager;
+        private SignInManager<ApplicationUser> signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty,"Invalid login attemp");
+                }
+            }
+            return View();
+        }
+        [Authorize]
+        public string Check()
+        {
+            return "you are logged";
+        }
+    }
+}
